3f45750da1615d1f0c58e843ceccdc77d4a110be
Magnus Aleksander Bilsted Rasch <magn507n@elev.tec.dk>
2023-11-05 16:14:29 +0100

    Program færdig

--- Employee_Crud_Blazor/Data/Employee.cs
+++ Employee_Crud_Blazor/Data/Employee.cs
@@ -9,6 +9,7 @@ namespace Employee_Crud_Blazor.Data
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+        public string FullName => $"{FirstName} {MiddleName} {LastName}";
         public string PostalCode { get; set; }
         public string Address { get; set; }
         public string EmailAddress { get; set; }
