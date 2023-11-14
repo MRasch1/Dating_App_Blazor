37048e2c6474b3cdf6fdadf95c35c1e7b6bb4f23
Mrasch1 <mrasch1102@gmail.com>
2023-11-14 11:32:24 +0100

    UserProfileService made

---
+++ Employee_Crud_Blazor/Data/UserProfileService.cs
@@ -0,0 +1,57 @@
+﻿using Employee_Crud_Blazor.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Crud_Blazor.Data
+{
+    public class UserProfileService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public UserProfileService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        //Get All UserProfile List
+        public async Task<List<UserProfile>> GetAllUserProfiles()
+        {
+            return await _applicationDbContext.UserProfile.ToListAsync();
+        }
+
+        //Add New UserProfile Record
+        public async Task<bool> AddNewUserProfile(UserProfile userProfile)
+        {
+            await _applicationDbContext.UserProfile.AddAsync(userProfile);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        //Get UserProfile Record by Id
+        public async Task<UserProfile> GetUserProfileById(int id)
+        {
+            UserProfile userProfile = await _applicationDbContext.UserProfile.FirstOrDefaultAsync(x => x.Id == id);
+            return userProfile;
+        }
+
+        public async Task<UserProfile> GetUserProfileByUsernameAsync(string username)
+        {
+            UserProfile userProfile = await _applicationDbContext.UserProfile.FirstOrDefaultAsync(u => u.UserName == username);
+            return userProfile;
+        }
+
+        //Update UserProfile Data
+        public async Task<bool> UpdateUserProfileDetails(UserProfile userProfile)
+        {
+            _applicationDbContext.UserProfile.Update(userProfile);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        //Delete UserProfile Data
+        public async Task<bool> DeleteUserProfile(UserProfile userProfile)
+        {
+            _applicationDbContext.UserProfile.Remove(userProfile);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
