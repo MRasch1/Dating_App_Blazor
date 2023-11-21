1d47d83d76ee29b32c95dafac894322736c86061
Mrasch1 <mrasch1102@gmail.com>
2023-11-21 11:22:15 +0100

    like method being implemented

---
+++ Employee_Crud_Blazor/Data/LikesService.cs
@@ -0,0 +1,28 @@
+﻿using Employee_Crud_Blazor.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Crud_Blazor.Data
+{
+    public class LikesService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public LikesService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        //Add New Likes Record
+        public async Task<bool> AddNewLikes(Likes likes)
+        {
+            await _applicationDbContext.Likes.AddAsync(likes);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Likes> GetLikesByLiker(int liker)
+        {
+            Likes likes = await _applicationDbContext.Likes.FirstOrDefaultAsync(x => x.Liker == liker);
+            return likes;
+        }
+    }
+}
